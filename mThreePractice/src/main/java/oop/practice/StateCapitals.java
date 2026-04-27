5f1844d04eeaf82359fe66f3f691a64e57df42ea
Alan Thomas <alanthomas171@gmail.com>
2026-04-27 16:43:01 +0100

    Start StateCapitals Task

---
+++ mThreePractice/src/main/java/oop/practice/StateCapitals.java
@@ -0,0 +1,25 @@
+package oop.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class StateCapitals {
+
+
+    public static void main(String[] args) {
+
+        Map<String, String> states = new HashMap<>();
+        states.put("Alabama", "Montgomery");
+        states.put("Alaska", "Pennsylvania");
+        states.put("Arizona", "Delaware");
+        states.put("Arkansas", "Delaware");
+
+        Set<String> keys = states.keySet();
+        
+
+
+    }
+
+
+}
