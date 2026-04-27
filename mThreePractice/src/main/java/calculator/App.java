5360921820096932ba924685e3300d4d45af7508
Alan Thomas <alanthomas171@gmail.com>
2026-04-27 02:34:05 +0100

    Tweaks/ Fix Loop logic

--- mThreePractice/src/main/java/calculator/App.java
+++ mThreePractice/src/main/java/calculator/App.java
@@ -9,34 +9,40 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         SimpleCalculator simpleCalculator = new SimpleCalculator();
+        while (true) {
+            System.out.println("pick what you want to do , 1 for add, 2 for sub, 3 for mult ,4 for div, 5 to exit");
+            int choice = sc.nextInt();
+            System.out.println("enter the first number");
+            double a = sc.nextDouble();
+            System.out.println("enter the second number");
+            double b = sc.nextDouble();
+            double res = 0;
 
-        System.out.println("pick what you want to do , 1 for add, 2 for sub, 3 for mult ,4 for div");
-        int choice =  sc.nextInt();
-        System.out.println("enter the first number");
-        double a = sc.nextDouble();
-        System.out.println("enter the second number");
-        double b = sc.nextDouble();
-        double res = 0;
+            switch (choice) {
+                case 1:
+                    res = simpleCalculator.add(a, b);
+                    break;
+                case 2:
+                    res = simpleCalculator.sub(a, b);
+                    break;
+                case 3:
+                    res = simpleCalculator.mul(a, b);
+                    break;
+                case 4:
+                    res = simpleCalculator.div(a, b);
+                    break;
+                case 5:
+                    System.out.println("thank you");
+                    System.exit(0);
+                default:
+                    System.out.println("invalid choice");
+                    continue;
 
-        switch (choice) {
-            case 1:
-                res = simpleCalculator.add(a, b);
-                break;
-            case 2:
-                res = simpleCalculator.sub(a, b);
-                break;
-            case 3:
-                res = simpleCalculator.mul(a, b);
-                break;
-            case 4:
-                res = simpleCalculator.div(a, b);
-                break;
-            default:
-                System.out.println("invalid choice");
+            }
 
+
+            System.out.println(res);
         }
-
-        System.out.println(res);
     }
 
 
