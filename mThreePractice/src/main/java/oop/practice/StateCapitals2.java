5cf6997156e0a508ee78e3fb8c2e0528e3c29b07
Alan Thomas <alanthomas171@gmail.com>
2026-04-28 16:17:45 +0100

    Read StateCapitals.txt file and parsed into hashmap

--- mThreePractice/src/main/java/oop/practice/StateCapitals2.java
+++ mThreePractice/src/main/java/oop/practice/StateCapitals2.java
@@ -1,6 +1,29 @@
 package oop.practice;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
 public class StateCapitals2 {
+    public static void main(String[] args) throws Exception{
+        Map<String, String> states = new HashMap<>();
+
+        Scanner scanner = new Scanner(new BufferedReader(new FileReader("StateCapitals.txt")));
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            System.out.println(line);
+            String[] parts = line.split("::");
+            states.put(parts[0], parts[1]);
+
+        }
+        System.out.println(states.size() + " STATES AND CAPITALS LOADED");
+
+    }
+
 
 
 }
