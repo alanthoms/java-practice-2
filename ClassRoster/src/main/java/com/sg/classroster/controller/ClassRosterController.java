8d95fbadd793ab4f067cf7c671744a03cdab2bd1
Alan Thomas <alanthomas171@gmail.com>
2026-04-30 16:06:27 +0100

    Class Roster Menu System

--- ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
+++ ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
@@ -1,4 +1,46 @@
 package com.sg.classroster.controller;
 
+import com.sg.classroster.ui.ClassRosterView;
+import com.sg.classroster.ui.UserIO;
+import com.sg.classroster.ui.UserIOConsoleImpl;
+
 public class ClassRosterController {
+
+    private UserIO io = new UserIOConsoleImpl();
+    private ClassRosterView view = new ClassRosterView();
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+            menuSelection = getMenuSelection();
+
+
+            switch (menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+
+        }
+        io.print("GOOD BYE");
+    }
+
+    private int getMenuSelection() {
+        return view.printMenuAndGetSelection();
+    }
 }
--- ClassRoster/src/main/java/com/sg/classroster/ui/ClassRosterView.java
+++ ClassRoster/src/main/java/com/sg/classroster/ui/ClassRosterView.java
@@ -1,4 +1,19 @@
 package com.sg.classroster.ui;
 
 public class ClassRosterView {
+    private UserIO io = new UserIOConsoleImpl();
+
+    public int printMenuAndGetSelection(){
+
+        io.print("Main Menu");
+        io.print("1. List Student IDs");
+        io.print("2. Create New Student");
+        io.print("3. View a Student");
+        io.print("4. Remove a Student");
+        io.print("5. Exit");
+
+        return io.readInt("Please select from the choices above." ,1,5);
+
+
+    }
 }
