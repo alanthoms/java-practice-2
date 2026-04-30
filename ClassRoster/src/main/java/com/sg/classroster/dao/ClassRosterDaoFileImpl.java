7a53d8cad436ce335e1150049021afb7e7c2592a
Alan Thomas <alanthomas171@gmail.com>
2026-04-30 23:27:26 +0100

    Class Roster Remove Student Use Case

--- ClassRoster/src/main/java/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
+++ ClassRoster/src/main/java/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
@@ -29,6 +29,8 @@ public class ClassRosterDaoFileImpl implements ClassRosterDao {
 
     @Override
     public Student removeStudent(String studentId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Student removedStudent = students.remove(studentId);
+        return removedStudent;
     }
+
 }
