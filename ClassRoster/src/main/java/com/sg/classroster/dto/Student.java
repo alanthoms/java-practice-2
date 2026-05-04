11446d11f2e8158f2ec27f8fd2de1733b5377539
Alan Thomas <alanthomas171@gmail.com>
2026-05-04 23:50:33 +0100

    Class Roster Add New Equals and Hashcode Override to Student

--- ClassRoster/src/main/java/com/sg/classroster/dto/Student.java
+++ ClassRoster/src/main/java/com/sg/classroster/dto/Student.java
@@ -1,6 +1,8 @@
 package com.sg.classroster.dto;
 
 
+import java.util.Objects;
+
 public class Student {
 
     private String firstName;
@@ -39,5 +41,42 @@ public class Student {
 
     public void setCohort(String cohort) {
         this.cohort = cohort;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.firstName);
+        hash = 89 * hash + Objects.hashCode(this.lastName);
+        hash = 89 * hash + Objects.hashCode(this.studentId);
+        hash = 89 * hash + Objects.hashCode(this.cohort);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.studentId, other.studentId)) {
+            return false;
+        }
+        if (!Objects.equals(this.cohort, other.cohort)) {
+            return false;
+        }
+        return true;
     }
 }
